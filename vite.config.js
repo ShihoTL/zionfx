098e7cb37003a55a9cdbbfee5dfc7e7149e2c32a
ShihoTL <mgbekenjustice2@gmail.com>
2025-05-15 17:03:09 +0000

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // <-- This is the critical fix
   plugins: [react()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
   server: {
     host: '0.0.0.0',
   }
-})
+});
